758c9c5fee2f658732f97cfee6373a18a0c6af40
Furkan Emre <furkan54emre@gmail.com>
2019-02-10 17:50:06 +0300

    Move player left right with  a and d

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal");
+        var newXPos = transform.position.x + deltaX;
+
+        transform.position = new Vector2(newXPos, transform.position.y);
     }
 }
