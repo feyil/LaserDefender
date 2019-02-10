f6ce699dd11842d7e5b603efee785979576fd290
Furkan Emre <furkan54emre@gmail.com>
2019-02-11 01:21:23 +0300

    WaveConfig scriptable object connected with prefabs

--- Assets/Scripts/WaveConfig.cs
+++ Assets/Scripts/WaveConfig.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Enemy Wave Config")]
 public class WaveConfig : ScriptableObject // Look there not MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+    [SerializeField] float spawnRandomFactor = 0.3f;
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float moveSpeed = 2f;
+
+    public GameObject GetEnemyPrefab()
     {
-        
+        return enemyPrefab;
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject GetPathPrefab()
     {
-        
+        return pathPrefab;
+    }
+
+    public float GetTimeBetweenSpawns()
+    {
+        return timeBetweenSpawns;
+    }
+
+    public float GetSpawnRandomFactor()
+    {
+        return spawnRandomFactor;
+    }
+
+    public int GetNumberOfEnemies()
+    {
+        return numberOfEnemies;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
     }
 }
