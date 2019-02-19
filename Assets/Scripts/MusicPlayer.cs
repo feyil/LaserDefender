cdaf4d203e58932765675d6a6ba472e97fbf2ede
Furkan Emre <furkan54emre@gmail.com>
2019-02-19 23:49:35 +0300

    Music Player with Singleton

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetUpSingleton()
+    {
+        // GetType get the type of this
+        if(FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
