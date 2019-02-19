b2af936cf184d09d43ce33f30745e9d9a563a6a2
Furkan Emre <furkan54emre@gmail.com>
2019-02-19 23:14:52 +0300

    Load All Scenes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        // it can cause errors string reference
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
