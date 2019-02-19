6e56970a755c57a1eb4cf1cf59c638c983f5e379
Furkan Emre <furkan54emre@gmail.com>
2019-02-19 13:32:06 +0300

    Layer Collision Matrix

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -26,13 +26,15 @@ public class Enemy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        if(!damageDealer) { return; }
         ProcessHit(damageDealer);
     }
 
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.GetDamage();
-        if(health <= 0)
+        damageDealer.Hit();
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -105,12 +105,14 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         DamageDealer damageDealer = collision.GetComponent<DamageDealer>();
+        if(!damageDealer) { return; }
         ProcessHit(damageDealer);
     }
 
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.GetDamage();
+        damageDealer.Hit();
         if (health <= 0)
         {
             Destroy(gameObject);
