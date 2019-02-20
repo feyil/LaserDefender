847e4568f79d2f768abd8329b15553f43dd930c6
Furkan Emre <furkan54emre@gmail.com>
2019-02-20 22:48:38 +0300

    Try and see fitting screen for 9:16 aspect ratio when it is builded

---
+++ Assets/CameraAspect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAspect : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Screen.SetResolution(360, 640, false);
+    }
+
+
+}
